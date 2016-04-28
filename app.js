eca57de6ba6b95e5b1f8f545b9da3a763ff08f6e
Ole Frank Jensen <olej@dr.dk>
2016-04-28 15:14:01 +0200

    add hosted db at mongolab

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://tiltuser:password@ds021691.mlab.com:21691/tiltdb');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
