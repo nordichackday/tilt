b0dc452f2ac442c9c49aed8a6beee2122f60b2ef
Ole Frank Jensen <olej@dr.dk>
2016-04-28 14:47:01 +0200

    add support for local mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../app/models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  var result = [];
+
+  // get all the users
+  User.find({}, function(err, users) {
+    if (err) throw err;
+
+    // object of all the users
+    console.log(users);
+    result = users;
+  });
+
+  res.send(result);
+
 });
 
 module.exports = router;
